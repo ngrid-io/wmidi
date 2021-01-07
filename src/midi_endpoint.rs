64b052c42f74d750e3aa701bfbb0acd1138e49c9
adamnemecek <adamnemecek@gmail.com>
2021-01-07 11:47:57 -0800

    stuff

--- src/midi_endpoint.rs
+++ src/midi_endpoint.rs
@@ -5,49 +5,53 @@ pub struct MIDIEndpoint {
 }
 
 impl MIDIEndpoint {
-    fn id(&self) -> u32 {
+    pub fn id(&self) -> u32 {
         // self.inner.set_property_integer(coremidi::kMIDIPropertyUniqueID, value);
         // self.inner.get_property_integer(coremidi::kMIDIPropertyUniqueID)
         self.inner.unique_id().unwrap()
     }
 
-    fn manufacturer(&self) -> String {
+    pub fn manufacturer(&self) -> String {
         // self.inner.as_ref().set_property_string(name, value)
         self.inner.manufacturer().unwrap()
     }
 
-    fn name(&self) -> String {
+    pub fn name(&self) -> String {
         self.inner.name().unwrap()
     }
 
     /// .input (for MIDIInput) or .output (for MIDIOutput)
-    fn kind(&self) -> MIDIPortKind {
+    pub fn kind(&self) -> MIDIPortKind {
         todo!()
     }
 
-    fn version(&self) -> u32 {
+    pub fn version(&self) -> u32 {
         todo!()
         // self.inner.ve
     }
 
     /// .connected | .disconnected,
     /// indicates if the port's endpoint is connected or not
-    fn state(&self) -> MIDIPortDeviceState {
+    pub fn state(&self) -> MIDIPortDeviceState {
         todo!()
     }
 
     /// .open | .closed
-    fn connection(&self) -> MIDIPortConnectionState {
+    pub fn connection(&self) -> MIDIPortConnectionState {
         todo!()
     }
 
     /// open the port, is called implicitly when MIDIInput's onMIDIMessage is set or MIDIOutputs' send is called
-    fn open(&mut self) {
+    pub fn open(&mut self) {
         todo!()
     }
 
     /// closes the port
-    fn close(&mut self) {
+    pub fn close(&mut self) {
         todo!()
+    }
+
+    pub fn flush(&self) {
+        let _ = self.inner.flush();
     }
 }
